99dfd90276b60487eeb8042ac1a9b4ed0ed525e1
luistorrest <luis.torres1@udea.edu.co>
2021-08-22 14:50:18 -0500

    Se agrega reconocimiento de color rosado

--- contours.py
+++ contours.py
@@ -1,28 +1,41 @@
+# Este fichero contiene una función que clasifica y ordena los contornos por color, tambien hace el cálculo
+# por donde tiene que pasar el Zagi
+
 import cv2
 
-def getContours(imgContour, img):
+def getContours(imgContour, img, label, listPositions):
     #almacena el listado de posiciciones para los contornos de los obstaculos
-    listPositions = []
+
     contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
         area = cv2.contourArea(cnt)
         print(area)
         if area>1000:
-            cv2.drawContours(imgContour, cnt, -1, (255, 0, 0), 3)
+            cv2.drawContours(imgContour, cnt, -1, (0, 0, 0), 5)
             peri = cv2.arcLength(cnt,True)
             approx = cv2.approxPolyDP(cnt,0.02*peri,True)
             print(len(approx))
             x, y, w, h = cv2.boundingRect(approx)
-            cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),2)
+            cv2.rectangle(imgContour,(x,y),(x+w,y+h),(0,255,0),15)
+            print('contorno: ', label)
             print('x: ', x, '\ny: ', y, '\nw: ', w, '\nh: ', h )
 
-            # Rectangulo direccion Zagi y posicionamiento del centro
-            listPositions.append(cv2.boundingRect(approx)) #listado de tuplas (posiciones de contornos)
-            try:
-                cv2.rectangle(imgContour, (listPositions[0][0], listPositions[0][1]), (listPositions[1][0],
-                                          (listPositions[1][3] + listPositions[1][1])), (0, 255, 0), 3)
-                cv2.circle(imgContour, (listPositions[0][0] + (listPositions[1][0] // 2),
-                                        (listPositions[0][1] + (listPositions[0][3]) // 2)), 20, (0, 0, 255),
-                           cv2.FILLED)
-            except IndexError:
-                print('indice fuera de rango')
+            #Coloca el nombre del color del contorno
+            cv2.putText(imgContour,label,
+                        (x+(w//2)-10,y+(h//2)-10),cv2.FONT_HERSHEY_COMPLEX,4,
+                        (0,255,255),14)
+
+            #listado de tuplas (posiciones de contornos)
+            listPositions.append(cv2.boundingRect(approx))
+
+    if label == 'PINK': #Se es el ultimo contorno entonces ...
+        #Orde todos los contornos en funcion de las coordenadas (x, y)
+        listPositions.sort(key=lambda x: x[0] + x[1])
+
+        # Rectangulo direccion Zagi y posicionamiento del centro
+        cv2.rectangle(imgContour, (listPositions[0][0], listPositions[0][1]),
+                      (listPositions[-1][0], listPositions[-1][1] + listPositions[-1][3]), (0, 255, 0), 15)
+
+        # Circulo posicionado en el centro del rectangulo
+        cv2.circle(imgContour,  ((listPositions[0][0]+listPositions[-1][0])//2,(listPositions[0][1]+(listPositions[-1][1]+listPositions[-1][3]))//2),
+               20, (0, 0, 255), cv2.FILLED)
--- main.py
+++ main.py
@@ -1,26 +1,43 @@
+# Antes de la competencia, ajustar los rangos de colores HSV con ayuda de los controles deslizables (sliders)
+# para obtener una máscara nítida en función de los colores del entorno que capture la camara del Zagi.
+
 import cv2
 import numpy as np
 from stack_images import stackImages
 from contours import getContours
 
-#Imagenes
+listPositions = []
+
+# Imagenes
 img = cv2.imread('images/obstaculo_verde.jpg')
 imgContour = img.copy()
-blurred_gaussian = cv2.GaussianBlur(img, (5, 5), 0)
+blurred_gaussian = cv2.GaussianBlur(img, (9,9), 2)
 imgHSV = cv2.cvtColor(blurred_gaussian, cv2.COLOR_BGR2HSV)
 imgAux = np.zeros_like(img)
 
-#Rangos HSV
-lower = np.array([42, 45, 119])
-upper = np.array([70, 255, 255])
+# Rangos HSV
+lowerGreen = np.array([43, 171, 72])
+upperGreen = np.array([70, 255, 255])
+lowerPink = np.array([145, 52, 106])
+upperPink = np.array([170, 255, 255])
 
-#Mascara
-mask = cv2.inRange(imgHSV, lower, upper)
+# Mascaras
+greenMask = cv2.inRange(imgHSV, lowerGreen, upperGreen)
+pinkMask = cv2.inRange(imgHSV, lowerPink, upperPink)
+mask = cv2.bitwise_or(greenMask, pinkMask)
+
+# Obtiene los contornos de las mascara clasificados por color
+# getContours(imgContour, mask)
+
+getContours(imgContour, greenMask, "GREEN", listPositions)
+getContours(imgContour, pinkMask, "PINK", listPositions)
+print(listPositions)
+
+# Interseccion de mascara e imagen orginal(BGR)
 result = cv2.bitwise_and(img, img, mask=mask)
-getContours(imgContour, mask)
 
-#Muestra matriz de imagenes
-imgStack = stackImages(0.3,([img, imgHSV, result], [mask, imgContour, imgAux]))
+# Muestra matriz de imagenes
+imgStack = stackImages(0.1,([img, imgHSV, result], [mask, imgContour, imgAux]))
 cv2.imshow('obstaculo verde', imgStack)
 
 cv2.waitKey()
